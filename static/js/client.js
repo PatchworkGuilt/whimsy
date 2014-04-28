33a38ae85d90ec3e546e6cce45a9a7ab734d4891
Scott Morse <scott@gamechanger.io>
2014-04-27 20:18:18 -0700

    fix broken shit from new physicsjs versino

--- static/js/client.js
+++ static/js/client.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var messages = [];
     var socket = io.connect(document.URL);
-    var paper = Raphael('viewport', 800, 800);
+    var paper = Raphael('viewport', 800, 400);
     socket.on('message', function (data) {
         paper.clear();
         var worldBodies = JSON.parse(data);

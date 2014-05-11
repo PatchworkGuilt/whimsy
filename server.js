c717784610351f8477986ec0568055e461a4e8c0
Scott Morse <scott@gamechanger.io>
2014-05-10 22:58:54 -0700

    now with ROOMS, well, a room.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var world = require('./static/js/world-engine.js');
+var requirejs = require("requirejs");
 var app = express();
 var port = 3700;
 
@@ -12,13 +12,23 @@ app.get("/", function(req, res){
 
 app.use(express.static(__dirname + '/static/'));
 
-var io = require('socket.io').listen(app.listen(port));
-io.set('log level', 2);
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', world.toJSON());
-    world.eventEmitter.on('tick', function (data) {
-        io.sockets.emit('message', world.toJSON());
+var FPS = 60.0;
+var timestep = 1000 / FPS;
+var room_id = "room1"; //Temporary, obviously
+
+requirejs(['./static/js/WorldEngine', 'static/js/util/StopWatch'], function(WorldEngine, StopWatch) {
+    var io = require('socket.io').listen(app.listen(port));
+    io.set('log level', 2);
+    var world;
+
+    function renderWorld(roomID, worldData){
+        io.sockets.in(roomID).emit('render', worldData);
+    };
+
+    io.sockets.on('connection', function (socket) {
+        socket.join(room_id);
+        if(!world)
+            world = new WorldEngine(timestep, renderWorld, room_id);
     });
 });
-
 console.log("Listening on port " + port);
